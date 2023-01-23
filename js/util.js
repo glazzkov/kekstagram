b390269854d0c651f30a5d7e5c36c070d69fbe0a
= <alexanderglazzkov@gmail.com>
2023-01-24 02:05:59 +0700

    рефакторинг утилитарных функций

--- js/util.js
+++ js/util.js
@@ -1,34 +1,29 @@
-const util = {
-  // возвращает случайное число от min до max
-  getRandomInt (min, max) {
-    if (min < 0 || max < 0) {
-      return -1;
-    }
-    if (min > max) {
-      [min, max] = [max, min];
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
-
-  // проверяет, является ли длина строки меньше переданной максимальной длины
-  isValidLength (string, maxLength) {
-    return (string.length <= maxLength);
-  },
-
-  // возвращает перемешанный массив уникальных чисел от 1 до указанного числа
-  getUniqueNumbersArray (length) {
-    let numbers = [];
-    for (let i = 1; i <= length; i++){
-      numbers.push(i);
-    }
-    for (let i = 0; i < length; i++){
-      let tempIndex = this.getRandomInt(0, length - 1);
-      let tempNumber = numbers[i];
-      numbers[i] = numbers[tempIndex];
-      numbers[tempIndex] = tempNumber;
-    }
-    return numbers;
-  },
+// возвращает случайное положительное число в указанном диапазоне
+export const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-export {util}
+// возвращает массив перемешанных чисел от нуля до указанной длины
+export const getUniqueNumbersArray = (length) => {
+  let numbers = [];
+  for (let i = 1; i <= length; i++){
+    numbers.push(i);
+  }
+  for (let i = 0; i < length; i++){
+    let tempIndex = getRandomInt(0, length - 1);
+    let tempNumber = numbers[i];
+    numbers[i] = numbers[tempIndex];
+    numbers[tempIndex] = tempNumber;
+  }
+  return numbers;
+}
+// универсальные булевые переменные
+export const bools = {
+  isEscKey: (key) => key === 'Escape' || key === 'Esc',
+  isValidLength: (string, maxLength) => (string.length <= maxLength),
+}
