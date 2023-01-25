fb94d348996ecb8ef271d0583c6236c7d3019029
= <alexanderglazzkov@gmail.com>
2023-01-26 04:37:46 +0700

    подключение модулей к странице

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import { getPictureObjectsArray } from './data.js';
 import { renderPictures } from './render.js';
+import { initUpload } from './image-upload.js'
+import {  initEffects } from './effects.js';
+import { initValidation } from './form-validation.js';
 
 const mainPicturesArray = getPictureObjectsArray();
 renderPictures(mainPicturesArray);
+initUpload();
+initEffects();
+initValidation();
