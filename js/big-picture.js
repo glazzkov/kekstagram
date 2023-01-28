aab2b23d5cb47dc14ac5225faa81b7b366343feb
= <alexanderglazzkov@gmail.com>
2023-01-28 21:52:52 +0700

    рефакторинг попапа с большим изображением

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,9 +1,12 @@
-import { bools } from './util.js';
+import { DOM, addCloseEvent } from './util.js';
 import { renderComments } from './render.js';
+import { getPictureById } from './data.js';
 
-// DOM-элементы для работы с большим изображением
-const body = document.querySelector('body');
-const bigPictureContainer = document.querySelector('.big-picture');
+// DOM-элементы попапа с большим изображением
+const body = DOM.body;
+const picturesContainer = DOM.picturesContainer;
+const bigPictureContainer = DOM.bigPictureContainer;
+// DOM-элементы для вставки данных большого изображения
 const bigPictureImage = bigPictureContainer.querySelector('.big-picture__img img');
 const pictureDescription = bigPictureContainer.querySelector('.social__caption');
 const likesCount = bigPictureContainer.querySelector('.likes-count');
@@ -12,39 +15,35 @@ const commentsCounter = bigPictureContainer.querySelector('.social__comment-coun
 const commentsLoader = bigPictureContainer.querySelector('.comments-loader');
 const closeButton = bigPictureContainer.querySelector('#picture-cancel');
 
-// обработчик события закрытия полноэкранного просмотра
-const onPictureClose = (evt) => {
-  evt.preventDefault();
-  if (bools.isEscKey(evt.key) || evt.type === 'click') {
-    bigPictureContainer.classList.add('hidden');
-    body.classList.remove('modal-open');
-    closeButton.removeEventListener('click', onPictureClose);
-    window.removeEventListener('keyup', onPictureClose);
-  }
+// вставка данных переданного изображения в попап с большой картинкой
+const insertData = (pictureInfo) => {
+  bigPictureImage.src = pictureInfo.url;
+  pictureDescription.textContent = pictureInfo.description;
+  likesCount.textContent = pictureInfo.likes;
+  commentsCount.textContent = pictureInfo.comments.length;
 }
 
-// инициализирует просмотр большого изображения для каждой миниатюры
-const initBigPicture = (picture, pictureInfo) => {
-  picture.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    bigPictureImage.src = pictureInfo.url;
-    pictureDescription.textContent = pictureInfo.description;
-    likesCount.textContent = pictureInfo.likes;
-    commentsCount.textContent = pictureInfo.comments.length;
-
-    renderComments(pictureInfo.comments, false);
-    closeButton.addEventListener('click', onPictureClose);
-    window.addEventListener('keyup', onPictureClose);
-
-    bigPictureContainer.classList.remove('hidden');
-    body.classList.add('modal-open');
-
-    //временное скрытие счетчика и загрузчика комментариев
-    commentsCounter.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
+// добавляет обработчик события нажатия на любую миниатюру, который открывает попап с большим изображением
+const addOpenEvent = () => {
+  picturesContainer.addEventListener('click', (evt) => {
+    const picture = evt.target.parentNode;
+    if (picture.classList.contains('picture')) {
+      evt.preventDefault();
+      const id = picture.id.replace('picture-', '');
+      const info = getPictureById(id);
+      insertData(info);
+      renderComments(info.comments, false);
+      bigPictureContainer.classList.remove('hidden');
+      body.classList.add('modal-open');
+    }
   });
-};
+}
 
-
-
-export { initBigPicture };
+// инициализация большого изображения при загрузке страницы, без показа и вставки данных
+export const initBigPicture = () => {
+  addCloseEvent(bigPictureContainer, closeButton);
+  addOpenEvent();
+  //временное скрытие счетчика и загрузчика комментариев
+  commentsCounter.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+}
