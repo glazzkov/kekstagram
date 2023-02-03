b24c862d775b2cb8713e723443fedaa90a9a532d
= <alexanderglazzkov@gmail.com>
2023-02-03 11:54:35 +0700

    Подключение всех модулей к проекту

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-import { initBigPicture } from './big-picture.js';
-import { initUpload } from './image-upload.js';
-import { initEffects } from './effects.js';
-import { initValidation } from './form-validation.js';
-import { getDataFromServer } from './api.js';
+// импорты компонентов
+import { Body } from './components/Body.js';
+import { Galery } from './components/Galery.js';
+import { PictureViewer } from './components/PictureViewer.js';
+import { UploadForm } from './components/UploadForm.js';
+import { Thumbnail } from './components/Thumbnail.js';
+import { Comment } from './components/Comment.js';
 
-// renderPictures(data);
-getDataFromServer();
-initBigPicture();
-initEffects();
-initValidation();
-initUpload();
+// регистрация компонентов
+customElements.define('app-body', Body, { extends: 'body' });
+customElements.define('app-galery', Galery, { extends: 'section' });
+customElements.define('app-picture-viewer', PictureViewer, { extends: 'section' });
+customElements.define('app-form', UploadForm, { extends: 'section' });
+customElements.define('app-thumbnail', Thumbnail, { extends: 'a' });
+customElements.define('app-comment', Comment, { extends: 'li' });
+
+
+document.$appBody = document.body;
