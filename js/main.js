ea6c3693e07fd0d05ee9a14e387546b566f0a611
= <alexanderglazzkov@gmail.com>
2023-02-14 22:55:19 +0700

    подключение всех модулей к проекту

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 // импорты компонентов
 import { Body } from './components/Body.js';
+import { ErrorMessage } from './components/ErrorMessage.js';
+import { SuccessMessage } from './components/SuccessMessage.js';
 import { Galery } from './components/Galery.js';
 import { PictureViewer } from './components/PictureViewer.js';
 import { UploadForm } from './components/UploadForm.js';
 import { Thumbnail } from './components/Thumbnail.js';
 import { Comment } from './components/Comment.js';
+document.$appBody = document.body;
 
 // регистрация компонентов
 customElements.define('app-body', Body, { extends: 'body' });
+customElements.define('app-error-message', ErrorMessage, { extends: 'section' });
+customElements.define('app-success-message', SuccessMessage, { extends: 'section' });
 customElements.define('app-galery', Galery, { extends: 'section' });
 customElements.define('app-picture-viewer', PictureViewer, { extends: 'section' });
 customElements.define('app-form', UploadForm, { extends: 'section' });
 customElements.define('app-thumbnail', Thumbnail, { extends: 'a' });
 customElements.define('app-comment', Comment, { extends: 'li' });
-
-
-document.$appBody = document.body;
