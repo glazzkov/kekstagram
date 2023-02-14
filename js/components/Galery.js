76c2adf591e6e708097e569bbd22ac5397585df8
= <alexanderglazzkov@gmail.com>
2023-02-14 22:53:22 +0700

    настройка получения данных галереи, показ ошибок

--- js/components/Galery.js
+++ js/components/Galery.js
@@ -1,15 +1,23 @@
 import { Thumbnail } from './Thumbnail.js';
+import { ErrorMessage } from './ErrorMessage.js';
 import { Api } from './Api.js';
 // класс галереи на главной
 export class Galery extends HTMLElement {
 
-  get $viewer() {return this._$viewer}
+  get $viewer() { return this._$viewer }
+  get $errorMessage() { return this._$errorMessage }
 
   constructor() {
     super();
     this.setAttribute('is', 'app-galery');
 
     this._$viewer = document.querySelector('.big-picture');
+    this._$errorMessage = new ErrorMessage(this.render);
+
+
+    this.$errorMessage.errorTitle = 'Не удалось загрузить данные с сервера';
+    this.$errorMessage.errorButtonText = 'Попробовать снова';
+    document.$appBody.appendChild(this.$errorMessage);
 
     this.addEventListener('click', (evt) => {
       const thumbnail = evt.target.parentNode;
@@ -24,13 +32,20 @@ export class Galery extends HTMLElement {
 
   get render() {
     return async () => {
-      const thumbnailsData = await Api.getPicturesData()
       const thumbnailsFragment = document.createDocumentFragment();
+      let thumbnailsData = [];
+      try {
+        thumbnailsData = await Api.getPicturesData()
+      } catch (error) {
+        this.$errorMessage.show();
+        return;
+      }
       thumbnailsData.forEach((thumbnailData) => {
         const thumbnail = new Thumbnail(thumbnailData);
         thumbnailsFragment.appendChild(thumbnail);
       });
       this.appendChild(thumbnailsFragment);
+      this.$errorMessage.hide();
     }
   }
 }
